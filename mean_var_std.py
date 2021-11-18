a273f5d906fc914dd353355ca1ba86617d126fb0
Sorusch Afkhami <sorusch.afkhami@gmail.com>
2021-11-18 09:28:26 +0100

    added ValueError rasied if list contains less than nine elements

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,11 @@
 import numpy as np
 
 def calculate(list):
-    arr = np.array([[list[0:3]], [list[3:6]], [list[6:9]]])
-
+    if len(list) < 9:
+        raise ValueError('List must contain nine numbers')
+    else:
+        arr = np.array([[list[0:3]], [list[3:6]], [list[6:9]]])
+        
 
     print(arr)
     #return calculations
