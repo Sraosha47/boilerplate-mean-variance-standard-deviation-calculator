0dca7a851ef6478a7ba224a5e2e84968f306a15b
Sorusch Afkhami <sorusch.afkhami@gmail.com>
2021-11-18 09:20:11 +0100

    Created 3 x 3 matrix out of list input into function

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,10 @@
 import numpy as np
 
 def calculate(list):
+    arr = np.array([[list[0:3]], [list[3:6]], [list[6:9]]])
 
 
+    print(arr)
+    #return calculations
 
-
-    return calculations
+calculate([1, 2, 3, 4, 5, 6, 7, 8, 9])
