a03da0100c16f93b78f518f16f204b2e7e6187dc
Sorusch Afkhami <sorusch.afkhami@gmail.com>
2021-11-18 09:40:52 +0100

    corrected the shape of the matrix

--- mean_var_std.py
+++ mean_var_std.py
@@ -4,10 +4,11 @@ def calculate(list):
     if len(list) < 9:
         raise ValueError('List must contain nine numbers')
     else:
-        arr = np.array([[list[0:3]], [list[3:6]], [list[6:9]]])
-        
+        arr = np.array([(list[0:3]), (list[3:6]), (list[6:9])])
+
 
     print(arr)
+    print(arr.shape)
     #return calculations
 
 calculate([1, 2, 3, 4, 5, 6, 7, 8, 9])
