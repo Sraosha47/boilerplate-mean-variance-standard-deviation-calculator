a8d4e0abbb1458e00141543b7847d1cda60a5ad0
Sorusch Afkhami <sorusch.afkhami@gmail.com>
2021-11-18 11:51:10 +0100

    finished the project

--- mean_var_std.py
+++ mean_var_std.py
@@ -2,13 +2,16 @@ import numpy as np
 
 def calculate(list):
     if len(list) < 9:
-        raise ValueError('List must contain nine numbers')
+        raise ValueError('List must contain nine numbers.')
     else:
         arr = np.array([(list[0:3]), (list[3:6]), (list[6:9])])
-
-
-    print(arr)
-    print(arr.shape)
-    #return calculations
-
-calculate([1, 2, 3, 4, 5, 6, 7, 8, 9])
+        flat_arr = arr.flatten()
+        calculations = {
+            "mean": [arr.mean(axis = 0).tolist(), arr.mean(axis=1).tolist(), flat_arr.mean().tolist()],
+            "variance": [arr.var(axis=0).tolist(), arr.var(axis=1).tolist(), flat_arr.var().tolist()],
+            "standard deviation": [arr.std(axis=0).tolist(), arr.std(axis=1).tolist(), flat_arr.std().tolist()],
+            "max": [arr.max(axis=0).tolist(), arr.max(axis=1).tolist(), flat_arr.max().tolist()],
+            "min": [arr.min(axis=0).tolist(), arr.min(axis=1).tolist(), flat_arr.min().tolist()],
+            "sum": [arr.sum(axis=0).tolist(), arr.sum(axis=1).tolist(), flat_arr.sum().tolist()]
+        }
+    return calculations
